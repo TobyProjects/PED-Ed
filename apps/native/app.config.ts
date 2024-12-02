97ffb0d81842c088c2959c8954b438d3b44a2c7f
Nguyen Minh Tho <definedentity@proton.me>
2024-12-02 18:14:29 +0700

    Update scheme

--- apps/native/app.config.ts
+++ apps/native/app.config.ts
@@ -6,7 +6,7 @@ const config: ExpoConfig = {
 	version: "1.0.0",
 	orientation: "portrait",
 	icon: "./src/assets/images/icon.png",
-	scheme: "ped-ed-app",
+	scheme: "peded",
 	userInterfaceStyle: "automatic",
 	newArchEnabled: true,
 	ios: {
